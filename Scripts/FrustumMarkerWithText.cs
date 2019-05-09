306f70a73d539a530afef3cfadea4b354624bcd3
Michele Kolodi <mykolodiy@gmail.com>
2019-05-09 11:10:28 +0200

    New marker

---
+++ Scripts/FrustumMarkerWithText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrustumMarkerWithText : FrustumMarker
+{
+    [SerializeField] Transform textAnchor;
+
+    private void Update()
+    {
+        textAnchor.transform.forward = -transform.parent.right;
+    }
+}
